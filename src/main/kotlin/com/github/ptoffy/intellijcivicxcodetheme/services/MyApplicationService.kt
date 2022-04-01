7000fb74cfa312bc2e8b20033a17ee696b1d47f7
GitHub Action <action@github.com>
2022-04-01 08:22:47 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/ptoffy/intellijcivicxcodetheme/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.ptoffy.intellijcivicxcodetheme.services
+
+import com.github.ptoffy.intellijcivicxcodetheme.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
